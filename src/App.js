e21a684b0bb4ae0aef41afe83b74a6b955b6336c
Olushola Henry <henryolusholasolomon@gmail.com>
2022-07-19 12:43:33 +0100

    added logic to save name and message in local storage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
 function App() {
+  const [msg, setMsg] = useState();
+  const [name, setName] = useState();
+  const [error, setError] = useState();
+  const [msgError, setMsgError] = useState();
+  const [hasName, setHasName] = useState(false);
+
+  const inputChangeHandler = (e) => {
+    setError(false);
+    const val = e.target.value;
+    val && setName(e.target.value);
+  };
+
+  const msgChangeHandler = (e) => {
+    setMsgError(false);
+    setMsg(e.target.value);
+  };
+
+  const nameSubmitHandler = () => {
+    if (name) {
+      const names = JSON.parse(localStorage.getItem("names"));
+      if (names) {
+        if (names.includes(name)) {
+          setError(true);
+          return;
+        }
+        localStorage.setItem("names", JSON.stringify([...names, name]));
+        setHasName(true);
+      } else {
+        localStorage.setItem("names", JSON.stringify([name]));
+        setHasName(true);
+      }
+    }
+  };
+
+  const sendMessageHandler = () => {
+    if (msg) {
+      const messages = JSON.parse(localStorage.getItem("messages"));
+      if (messages) {
+        localStorage.setItem(
+          "messages",
+          JSON.stringify([...messages, { name: name, message: msg }])
+        );
+      } else {
+        localStorage.setItem(
+          "messages",
+          JSON.stringify([{ name: name, message: msg }])
+        );
+      }
+      setMsg("");
+    } else {
+      setMsgError(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!hasName && (
+        <>
+          <label>Enter your name</label>
+          {error && <p>The name entered already exists</p>}
+          <input name="name" onChange={inputChangeHandler} />
+          <button onClick={nameSubmitHandler}>Submit</button>
+        </>
+      )}
+      {hasName && (
+        <>
+          <label>Message</label>
+          <input name="message" onChange={msgChangeHandler} value={msg || ""} />
+          <button onClick={sendMessageHandler}>Send</button>
+        </>
+      )}
+    </>
   );
 }
 
